568993c476a12e46653ef8b8ee13b88ea5006965
Parallels <morrisyau@college.harvard.edu>
2016-08-29 19:56:57 -0400

    added constraint to first state velocity

--- drake/systems/plants/test/fallingBrickLCP.m
+++ drake/systems/plants/test/fallingBrickLCP.m
@@ -11,9 +11,8 @@ p = TimeSteppingRigidBodyManipulator(s,.01,options);
 p = p.addRobotFromURDF(s,[],[],options);
 % x0 = [0;1;2;rpy2quat(randn(3,1));randn(6,1)];
 x0 = [0;1;2;rpy2quat(randn(3,1));2;1;2;rpy2quat(randn(3,1));randn(12,1)];
-start = x0;
 x0 = p.resolveConstraints(x0);
-
+start = x0;
 if 0
     v = p.constructVisualizer();
     sys = cascade(p,v);
@@ -26,10 +25,14 @@ v.drawWrapper(0,x0);
 
 
 %You should support this functionality
-N = 50;
-cto = ContactTrajectoryOptimization(p,N,[0.5 1.5]);
+N = 20;
+cto = ContactTrajectoryOptimization(p,N,[2.0 2.5]);
 cto = cto.addStateConstraint(ConstantConstraint(start(1:24)),1);
-xtraj = p.simulate([0 1.0],x0);
+xtraj = p.simulate([0 2.25],x0);
+x_old = xtraj;
+
+v.playback(xtraj);
+
 [m,n] = size(xtraj.xx);
 xf = xtraj.xx(:,n);
 cto = cto.addStateConstraint(ConstantConstraint(xf),N);
@@ -40,17 +43,28 @@ cto = cto.addRunningCost(@cost);
         g = sum((R*u).*u,1);
         dg = [zeros(1,1+size(x,1)),2*u'*R];
     end
-tf0 = 1.0;
-traj_init.x = PPTrajectory(foh([0,tf0],[double(start(1:24)),double(xf)]));
+tf0 = 2.25;
+traj_init.x = PPTrajectory(foh([0,tf0],[double(start),double(xf)]));
 
  for attempts=1:10
      tic
      [xtraj,utraj,z,F,info] = cto.solveTraj(tf0,traj_init);
      toc
-     if info==1 || info == 3, break; end
+     if info==1 || info == 3 || info == 41, break; end
  end
 
 
+%  for i = 1:N-2
+%      a = 24*(i-1);
+%      b = a + 24;
+%      c = b + 24;
+%      x0 = xtraj.pp.coefs(a+1:a+12,2);
+%      x1 = xtraj.pp.coefs(b+1:b+12,2);
+%      x2 = xtraj.pp.coefs(c+1:c+12,2);
+%      h0 = xtraj.pp.breaks(2);
+%      h1 = xtraj.pp.breaks(2);
+%      f = cto.forward_constraint_fun(h0,h1,x0,x1,x2);
+%  end
 
 %xtraj = p.simulate([0 0.5],x0);
 
